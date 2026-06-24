9800d487546471c817e7347265f496c6bfe313d7
OneLevel Studio <eliott.1421@gmail.com>
2026-06-24 03:34:20 +0200

    Update MenuScene.js

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -3,20 +3,16 @@
 // =========================================================
 import { gameVolume, playerCoins } from "../globals.js";
 import { createButton } from "../utils/helpers.js";
-
 export class MenuScene extends Phaser.Scene {
   constructor() { super("MenuScene"); }
   create() {
     const { width, height } = this.scale;
-
-    this.add.text(width / 2, 120, "Jumpix", {
+    this.add.text(width / 2, 80, "Jumpix", {
       fontSize: "50px", color: "#ffffff", fontStyle: "bold"
     }).setOrigin(0.5);
-
     this.coinText = this.add.text(width - 20, 20, `💰 ${playerCoins}`, {
       fontSize: "28px", color: "#ffff00", fontStyle: "bold"
     }).setOrigin(1, 0);
-
     const buttons = [
       { label: "➡ Play !!", scene: "World1"         },
       { label: "Shop 🛒",   scene: "ShopScene"       },
@@ -25,22 +21,18 @@ export class MenuScene extends Phaser.Scene {
       { label: "🛠 Level Editor", scene: "LevelEditorScene" },
       { label: "⚔ Duel local", scene: "MinigameScene" },
     ];
-
     let startY = 185;
     buttons.forEach((btn, i) => {
       createButton(this, width / 2, startY, 320, 60, btn.label, () => {
         this.sound.play("select", { volume: gameVolume });
         this.scene.start(btn.scene);
       }, i);
-      startY += 62;
+      startY += 78;
     });
-
     createButton(this, 40, 40, 60, 50, "⚙", () => {
       this.sound.play("menu", { volume: gameVolume });
       this.scene.start("SettingsScene");
     });
-
-
     createButton(this, 110, 40, 60, 50, "🏆", () => {
       this.sound.play("menu", { volume: gameVolume });
       this.scene.start("LeaderboardScene");
